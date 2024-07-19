79f7439abc734adcc893e11cba602c15bf088732
Thi-Nac <148755595+Thi-Nac@users.noreply.github.com>
2024-07-19 13:31:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const profilePic = document.querySelector('.profile-pic');
+    profilePic.addEventListener('click', function() {
+        alert('Profile Picture Clicked!');
+    });
+});
